3ea7c66ecc286a8144f3d4768f5d796b39a3117c
Sarah Enaile <sarahenaile@gmail.com>
2026-01-12 10:00:00 -0300

    fix: fix repositorys

--- movies/src/main/java/dev/sarah/movies/repositorys/MovieRepository.java
+++ movies/src/main/java/dev/sarah/movies/repositorys/MovieRepository.java
@@ -1,17 +1,15 @@
-package dev.sarah.movies.repositorys;
+package dev.sarah.movies.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository; 
 import org.springframework.stereotype.Repository;
 
-import dev.sarah.movies.Domain.Movies.Entitie.Movie;
+import dev.sarah.movies.domain.movies.entities.Movie;
 
 import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
-
     Optional<Movie> findByImdbId(String imdbId);
-
 
 }
